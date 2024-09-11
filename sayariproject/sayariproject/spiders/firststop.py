168bc3b0e7fec9c0a44ab8f5c0d820619c123133
LukeAlbarracin <lalbarracin2000@gmail.com>
2024-09-11 16:13:35 -0500

    Basic graph visualization w/o Atlas

--- sayariproject/sayariproject/spiders/firststop.py
+++ sayariproject/sayariproject/spiders/firststop.py
@@ -1,4 +1,6 @@
 import scrapy
+from pydispatch import dispatcher
+from scrapy import signals
 import json
 import networkx as nx
 import matplotlib.pyplot as plt
@@ -6,9 +8,12 @@ import matplotlib.pyplot as plt
 class FirststopSpider(scrapy.Spider):
     name = "firststop"
     allowed_domains = ["firststop.sos.nd.gov"]
-    start_urls = ["https://firststop.sos.nd.gov/api/Records/businesssearch", "https://firststop.sos.nd.gov/api/WebUserAccess/GET_USER_IS_OWNER"]
+    start_urls = ["https://firststop.sos.nd.gov/api/Records/businesssearch"]
     headers = {'Content-Type': 'application/json','authorization': 'undefined','Accept': 'application/json'}
-    G = nx.Graph() # Person or Company that is an agent / owner
+    company_info = []
+
+    def __init__(self):
+        dispatcher.connect(self.spider_closed, signals.spider_closed)
 
     def start_requests(self):
         payload = json.dumps({
@@ -16,7 +21,6 @@ class FirststopSpider(scrapy.Spider):
             "STARTS_WITH_YN": "true",
             "ACTIVE_ONLY_YN": True
         })
-        self.log("Begin requests...")
         yield scrapy.Request(
             url=self.start_urls[0], # Business Search
             method='POST',
@@ -29,21 +33,9 @@ class FirststopSpider(scrapy.Spider):
         for key in data:
             company = data[key]
             source_id = company["ID"]
-            print(f"CompanyID: {source_id}")
-            print("\n")
-            payload = json.dumps({
-                "SOURCE_TYPE_ID": 54, 
-                "SOURCE_ID": 337397
-            })
-            # yield scrapy.Request(
-            #     url=self.start_urls[1], # Confirm if user is owner,
-            #     method='POST',
-            #     body=payload,
-            #     headers=self.headers,
-            #     callback=self.parse_company
-            # )
+            self.log(f"CompanyID: {source_id}\n")
             company_name = company["TITLE"][0]
-            url = "https://firststop.sos.nd.gov/api/FilingDetail/business/103327/false"
+            url = f"https://firststop.sos.nd.gov/api/FilingDetail/business/{source_id}/false"
             yield scrapy.Request(
                 url=url,
                 method='GET',
@@ -51,33 +43,15 @@ class FirststopSpider(scrapy.Spider):
                 callback=self.parse_company,
                 meta={'Company': company_name}
             )
-            ## Also include handle error
     
     def parse_company(self, response):
-        self.log("Done here...")
         data = response.json()["DRAWER_DETAIL_LIST"]
         flattened_dict = {entry['LABEL']: entry['VALUE'] for entry in data}
-        print(flattened_dict)
-        # self.G.add_node("")
         company_name = response.meta.get("Company")
-        self.G.add_node(company_name, type="Company")
-        if "Commercial Registered Agent" in flattened_dict:
-            cr_agent = flattened_dict["Commercial Registered Agent"].split("\n")[0]
-            self.G.add_node(cr_agent, type="Person")
-            self.G.add_edges_from([(company_name, cr_agent),])
-        
-        pos = nx.spring_layout(self.G)
-        nx.draw_networkx_nodes(self.G, pos, node_size=3000)
-        nx.draw_networkx_edges(self.G, pos, edgelist=self.G.edges(), arrows=True)
-        nx.draw_networkx_labels(self.G, pos)
-        plt.title('Company and Owner Network')
-        plt.show()
+        flattened_dict["COMPANY"] = company_name
+        self.company_info.append(flattened_dict)
         # MUST USE ITEMS, see: https://www.geeksforgeeks.org/how-to-use-scrapy-items/
-        
-            
-
-
-
-
-            
-
+    
+    def spider_closed(self, spider):
+        with open('output.json', 'w') as f:
+            json.dump(self.company_info, f, indent=4)
